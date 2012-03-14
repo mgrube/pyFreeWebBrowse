aed9ca9f86dba94f5130f86de2f7e02438520704
James Valleroy <james.valleroy@gmail.com>
2012-03-14 18:13:42 -0400

    Added simple url left-side longest match for rules

--- freewebnode.py
+++ freewebnode.py
@@ -24,17 +24,41 @@ class FreeWebNode:
 
 	def checkAllFriendRequests(self):
 		for friend in self.friendList:
-			if 'key' in friend:
-				self.checkRequestPage(friend['key'])
+			if u'key' in friend:
+				self.checkRequestPage(friend)
 		pass
 
-	def checkRequestPage(self, requestPageUri):
-		# fetch page at requestPageUri
+	def checkRequestPage(self, friend):
+		# fetch page at friend[u'key']
 		# parse request list
 		# for each request
-			# if rules allow it,
-				# fulfillRequest(url)
+			# if checkRules(friend[u'name'], requestUrl):
+				# fulfillRequest(requestUrl)
+			# TODO: forward this request to other friends if not fulfilled here?
 		pass
+
+	# returns true if allowed, false if denied
+	def checkRules(self, friendName, requestUrl):
+
+		# check if we can match the whole url
+		if requestUrl in self.rules:
+			rule = self.rules[requestUrl]
+			if rule[u'allow_any'] == 'y':
+				return True
+			if friendName in rule[u'allow_list']:
+				return True
+			return False
+
+		# check to match longest left-side portion of the url
+		for i in range(len(requestUrl)):
+			if requestUrl[:-(i+1)] in self.rules:
+				rule = self.rules[requestUrl[:-(i+1)]]
+				if rule[u'allow_any'] == 'y':
+					return True
+				if friendName in rule[u'allow_list']:
+					return True
+				return False
+		return False
 
 	def fulfillRequest(self, url):
 		# urllib.urlretrieve(url, dest)
