9d173d30cc4aa52f88088009b366158908fbaaba
James Valleroy <james.valleroy@gmail.com>
2012-04-19 22:23:38 -0400

    Publish request page

--- addRequest.py
+++ addRequest.py
@@ -3,13 +3,17 @@
 import sys, time, json
 import os, errno
 
+from freesitetools import *
 from fcp.sitemgr import SiteMgr, fixUri
 
 # add a request for a web site to be uploaded to freenet
 def addRequest(url):
+	sitename = 'fwb-requests'
+	sitemgr = SiteMgr()
+
 	# read current request list
 	try:
-		f = open('fwb-requests/requests.json', 'r')
+		f = open(sitename + '/requests.json', 'r')
 		f_text = f.read()
 		f.close()
 		requestList = json.loads(f_text)
@@ -26,20 +30,51 @@ def addRequest(url):
 	# write new request list to disk
 	text = json.dumps(requestList)
 	try:
-		os.makedirs('fwb-requests')	# create folder
+		os.makedirs(sitename)		# create folder
 	except OSError, e:
 		if e.errno != errno.EEXIST:	# folder already exists
 			raise
-	f = open('fwb-requests/requests.json', 'w')
+	f = open(sitename + '/requests.json', 'w')
 	f.write(text)
 	f.close()
 
-	# TODO: add or update freesite for request list
-	pass
+	# add index if it doesn't already exist
+	try:
+		f = open(sitename + '/index.html', 'r')
+	except IOError as e:
+		f = open(sitename + '/index.html', 'w')
+		f.write('''
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+<head>
+	<title>pyFreeWebBrowse Request Page</title>
+</head>
+
+<body>
+	<a href="requests.json">Requests</a>
+</body>
+</html>
+			''')
+		f.close()
+
+	# add or update freesite for request list
+	if sitename in getFreesiteNames(sitemgr):
+		updateFreesite(sitemgr, sitename)
+	else:
+		uriPub = createFreesite(sitemgr, sitename, sitename)
+		return uriPub
+
 
 if __name__ == "__main__":
 	if (len(sys.argv) == 1):
 		print "Usage: ./addRequest <url>"
 	else:
-		addRequest(sys.argv[1])
+		uriPub = addRequest(sys.argv[1])
+		if uriPub == None:
+			print '\nPublished request'
+		else:
+			print '\nPublished request to:', uriPub
+			f = open('request-key.txt', 'w')
+			f.write(uriPub)
+			f.close()
 
